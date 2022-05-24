df53944d4237db46a416805e76fd374254b904c5
Antar-chakrabarti <chakrabartiantar@gmail.com>
2022-05-24 18:43:41 +0600

    update more

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -18,11 +18,28 @@ async function run(){
 try{
     await client.connect();
     const productsCollection = client.db('bike_parts').collection('products');
+    const placeOrderCollection = client.db('bike_parts').collection('place_orders');
 
     app.get('/products', async(req, res)=>{
         const query = {};
         const products = await productsCollection.find(query).toArray();
         res.send(products)
+    });
+    app.get('/products/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const product = await productsCollection.findOne(query)
+        res.send(product)
+    })
+    app.post('/place-order', async(req, res)=>{
+        const parts = req.body;
+        const query = {name: parts.name, email: parts.user}
+        const exists = await placeOrderCollection.findOne(query);
+        if(exists){
+            return res.send({success: false, parts: exists})
+        }
+        const result = placeOrderCollection.insertOne(parts);
+        res.send(result)
     })
 }
 finally{
